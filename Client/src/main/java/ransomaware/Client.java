1c372ea0fe18bb29716a3da73c7973383a09d6fd
Daniel Gonçalves <daniel.gdf99@gmail.com>
2020-11-13 18:14:17 +0000

    Add file reading and enconding to base64

--- Client/src/main/java/ransomaware/Client.java
+++ Client/src/main/java/ransomaware/Client.java
@@ -4,6 +4,9 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.security.cert.Certificate;
 import java.io.*;
+import java.nio.file.*;
+import java.util.Base64;
+import com.google.gson.*;
 
 import javax.net.ssl.*;
 
@@ -33,8 +36,11 @@ public class Client {
                     getFile(args);
                     break;
                 // send file.txt
-                case ("send"):
-                    sendFile(args);
+                case ("save"):
+                    saveFile(args);
+                    break;
+                case ("help"):
+                    // TODO: Show commands
                     break;
                 default:
                     System.out.println("Command not found.");
@@ -49,47 +55,78 @@ public class Client {
             return;
         }
         String file[] = args[1].split("/"), user = "", filename = "";
-        if (file.length == 1) { user = "me"; filename = file[0]; }
+        if (file.length == 1) { user = ""; filename = file[0]; }
         else if (file.length == 2) { user = file[0]; filename = file[1]; }
+
+        try {
+            // TODO: Send request to get file
+            // TODO: Write file
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
 
         System.out.println(user + " - " + filename);
     }
 
-    public static void sendFile(String[] args) {
+    /**
+     * saveFile
+     * @param args - for example: ['save','a.txt'] or ['save','masterzeus','a.txt']
+     */
+    public static void saveFile(String[] args) {
         if (args.length == 1 || args.length > 2) {
-            System.out.println("send: Too many arguments.\nExample: send a.txt");
+            System.out.println("save: Too many arguments.\nExample: save a.txt");
             return;
         }
-        String file[] = args[1].split("/"), user = "", filename = "";
-        if (file.length == 1) { user = "me"; filename = file[0]; }
-        else if (file.length == 2) { user = file[0]; filename = file[1]; }
+        String givenFile[] = args[1].split("/"), user = "", filename = "";
+        if (givenFile.length == 1) { user = ""; filename = givenFile[0]; }
+        else if (givenFile.length == 2) { user = givenFile[0]; filename = givenFile[1]; }
 
-        System.out.println(user + " - " + filename);
+        try {
+            String filePath = ClientVariables.FS_PATH + '/' + user + '/' + filename;
+
+            // Check if file exists
+            File file = new File(filePath);
+            if (!file.exists()) {
+                System.out.println("File not found.");
+                return;
+            }
+
+            // Read file to bytes
+            byte[] data = Files.readAllBytes(Path.of(filePath));
+
+            // Pass string file to base64
+            String encodedData = SecurityUtils.getBase64(data);;
+
+            // Create JSON
+            JsonObject jsonRoot = JsonParser.parseString("{}").getAsJsonObject();
+            jsonRoot.addProperty("data", encodedData);
+
+            System.out.println(jsonRoot);
+
+            // Send file
+            // TODO: Send request
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public static String requestGetFromURL(String url) {
         try {
             // TODO: use custom keystore if desired
-//            System.setProperty("javax.net.ssl.trustStore", ClientVariables.KEYSTORE);
-//            System.setProperty("javax.net.ssl.trustStorePassword", ClientVariables.SSL_STOREPASS);
+            // System.setProperty("javax.net.ssl.trustStore", ClientVariables.KEYSTORE);
+            // System.setProperty("javax.net.ssl.trustStorePassword", ClientVariables.SSL_STOREPASS);
             URL myUrl = new URL(url);
             HttpsURLConnection conn = (HttpsURLConnection) myUrl.openConnection();
 
-            // We bypass the hostname verifier, since we don't use a root CA
-            conn.setHostnameVerifier(new HostnameVerifier() {
-                public boolean verify(String hostname, SSLSession session) {
-                    return true;
-                }
-            });
-            InputStream is = conn.getInputStream();
-            InputStreamReader isr = new InputStreamReader(is);
-            BufferedReader br = new BufferedReader(isr);
+            BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
 
             String inputLine;
             StringBuilder output = new StringBuilder();
 
             while ((inputLine = br.readLine()) != null) {
-                // System.out.println(inputLine);
                 output.append(inputLine + '\n');
             }
 
