61facb030ea1d223317f63a1d07380c7c08939a6
Daniel Gonçalves <daniel.gdf99@gmail.com>
2020-12-09 23:30:15 +0000

    Add unit tests for session manager

--- Server/src/test/java/ransomaware/BaseIT.java
+++ Server/src/test/java/ransomaware/BaseIT.java
@@ -1,22 +1,15 @@
 package ransomaware;
 
-import com.google.gson.JsonObject;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 
 import java.io.IOException;
-import java.net.URI;
 import java.net.UnknownHostException;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
 import java.util.List;
 import java.util.Properties;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import org.apache.commons.lang3.RandomStringUtils;
 
 
 public class BaseIT {
@@ -25,8 +18,6 @@ public class BaseIT {
     protected static Properties testProps;
     private static MongoClient mongoClient;
     private static String dbName;
-    private static final ExecutorService executor = Executors.newSingleThreadExecutor();
-    private static HttpClient client = HttpClient.newBuilder().executor(executor).build();
     static String baseUrl;
 
     @BeforeAll
@@ -60,6 +51,7 @@ public class BaseIT {
         String HOST = testProps.getProperty("server.host");
         String PORT = testProps.getProperty("server.port");
         baseUrl = "https://" + HOST + ":" + PORT;
+        ServerVariables.init("ransom-aware", "mongodb://localhost:27017", "localhost:rsync/");
     }
 
     @AfterAll
@@ -68,34 +60,10 @@ public class BaseIT {
         mongoClient.dropDatabase(dbName);
 
         mongoClient.getDB(dbName);
-        executor.shutdownNow();
-        client = null;
         System.gc();
     }
 
     // Helper methods
-    String requestPostFromURL(String url, JsonObject jsonObject) throws IOException, InterruptedException {
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(url))
-                .header("Content-Type", "application/json")
-                .header("Cache-Control", "no-store")
-                .POST(HttpRequest.BodyPublishers.ofString(jsonObject.toString()))
-                .build();
-        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-
-        return response.body();
-    }
-
-    String requestGetFromURL(String url, HttpClient client) throws IOException, InterruptedException {
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(url))
-                .build();
-
-        HttpResponse<String> response =
-                client.send(request, HttpResponse.BodyHandlers.ofString());
-
-        return response.body();
-    }
 
     String generateRandomString(int length, boolean useLetters, boolean useNumbers) {
         return RandomStringUtils.random(length, useLetters, useNumbers);
---
+++ Server/src/test/java/ransomaware/SessionManagerUnitTest.java
@@ -0,0 +1,124 @@
+package ransomaware;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import org.junit.jupiter.api.*;
+import ransomaware.exceptions.DuplicateUsernameException;
+import ransomaware.exceptions.InvalidUserNameException;
+import ransomaware.exceptions.UnauthorizedException;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.fail;
+
+@DisplayName("Test Register and Login functions")
+public class SessionManagerUnitTest extends BaseIT {
+
+    // static members
+    static String registeredUsername;
+    static String registeredPassword;
+    static final String illegalUsername = "daniel/gon";
+    static String loggedSessionToken;
+
+
+    // one-time initialization and clean-up
+    @BeforeAll
+    public static void oneTimeSetUp(){
+        ServerVariables.init("ransom-aware", "mongodb://localhost:27017", "localhost:rsync/");
+    }
+
+    @AfterAll
+    public static void oneTimeTearDown() {
+
+    }
+
+    // initialization and clean-up for each test
+
+    @BeforeEach
+    public void setUp() {
+
+    }
+
+    @AfterEach
+    public void tearDown() {
+
+    }
+
+    // tests
+
+    /**
+     * Register Tests
+     * Note: all usernames in these tests have at least 10 characters
+     */
+    @Test
+    @DisplayName("Register with random user and password")
+    public void RegisterRandom() {
+        try {
+            registeredUsername = generateRandomString(10, true, true);
+            registeredPassword = generateRandomString(10, true, true);
+            SessionManager.register(registeredUsername, registeredPassword, "fake-cert", "fake-cert-2");
+        } catch (Exception e) {
+            fail("Should not have thrown exception");
+        }
+    }
+
+    @Test
+    @DisplayName("Register the previous one already registered")
+    public void TryRegisterAlreadyRegistered() {
+        assertThrows(DuplicateUsernameException.class, () -> {
+            SessionManager.register(registeredUsername, registeredPassword, "fake-cert", "fake-cert-2");
+        });
+    }
+
+    @Test
+    @DisplayName("Register with illegal characters in name")
+    public void RegisterIllegalChars() {
+        assertThrows(InvalidUserNameException.class, () -> {
+            SessionManager.register(illegalUsername, registeredPassword, "fake-cert", "fake-cert-2");
+        });
+    }
+
+    /**
+     * Login Tests
+     */
+    @Test
+    @DisplayName("Login with registered user")
+    public void LoginRegistered() {
+        try {
+            loggedSessionToken = SessionManager.login(registeredUsername, registeredPassword);
+        } catch (Exception e) {
+            fail("Should not have thrown exception");
+        }
+    }
+
+    @Test
+    @DisplayName("Login with registered user but wrong password")
+    public void LoginWrongPassword() {
+        assertThrows(UnauthorizedException.class, () -> {
+            SessionManager.login(registeredUsername, "NotThisOneForSure,AmIRight?");
+        });
+    }
+
+    @Test
+    @DisplayName("Login with non existent user")
+    public void LoginNonExistentUser() {
+        assertThrows(UnauthorizedException.class, () -> {
+            SessionManager.login("zeus", "NotThisOneForSure,AmIRight?");
+        });
+    }
+
+    // Helper
+    JsonObject formRegisterReq(String username, String password) {
+        JsonObject jsonRequest = JsonParser.parseString("{}").getAsJsonObject();
+        jsonRequest.addProperty("username", username);
+        jsonRequest.addProperty("password", password);
+        return jsonRequest;
+    }
+
+    JsonObject formRegisterReq(String username) {
+        return formRegisterReq(username, generateRandomString(10, true, true));
+    }
+}
+
